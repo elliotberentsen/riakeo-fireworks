069db070f623a2d7a8b2ea756cdeae35efd31d71
Elliot Berentsen <elliotberentsen@users.noreply.github.com>
2026-01-21 16:45:29 +0100

    fix

--- src/components/RetailersSection.tsx
+++ src/components/RetailersSection.tsx
@@ -39,7 +39,7 @@ export default function RetailersSection({ retailers }: RetailersSectionProps) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/512/{z}/{x}/{y}?access_token=${mapboxToken}`}
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/512/{z}/{x}/{y}?access_token=${mapboxToken}`}
           tileSize={512}
           zoomOffset={-1}
         />
